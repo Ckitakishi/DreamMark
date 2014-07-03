d7c506be159300dd8cb3991c39e8bc97556bdd62
Chenyh <me@ckitakishi.com>
2014-07-03 09:34:00 +0800

    更新
    涂鸦功能还差分享。

--- DreamMark/DMGraffitiViewController.h
+++ DreamMark/DMGraffitiViewController.h
@@ -9,9 +9,18 @@
 #import <UIKit/UIKit.h>
 
 @interface DMGraffitiViewController : UIViewController
+- (IBAction)acitonShare:(id)sender;
+- (IBAction)actionSave:(id)sender;
+- (IBAction)buttonEraser:(id)sender;
+- (IBAction)buttonM:(id)sender;
+- (IBAction)buttonA:(id)sender;
+- (IBAction)buttonE:(id)sender;
+- (IBAction)actionR:(id)sender;
+@property (weak, nonatomic) IBOutlet UIButton *eraser;
+- (IBAction)actionD:(id)sender;
 - (IBAction)graffitiCancel:(id)sender;
 @property (weak, nonatomic) IBOutlet UIView *myView;
-@property (weak, nonatomic) IBOutlet UIImageView *graffitiView;
+@property (weak, nonatomic) IBOutlet UIImageView *Canvas;
 @property (weak, nonatomic) IBOutlet UIButton *shareButton;
 @property (weak, nonatomic) IBOutlet UIButton *saveButton;
 
--- DreamMark/DMGraffitiViewController.m
+++ DreamMark/DMGraffitiViewController.m
@@ -9,11 +9,19 @@
 #import "DMGraffitiViewController.h"
 
 @interface DMGraffitiViewController ()
+{
+    CGFloat  fGreen;
+    CGFloat  fRed;
+    CGFloat  fBlue;
+    BOOL bRubber;
+    CGPoint  pntBegin;
+    BOOL bMoved;
+}
 
 @end
 
 @implementation DMGraffitiViewController
-
+int fBrushWidth=5;
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
@@ -29,8 +37,88 @@
     // Do any additional setup after loading the view.
     [self.saveButton setBackgroundImage:[UIImage imageNamed:@"save.png"] forState:UIControlStateNormal];
     [self.shareButton setBackgroundImage:[UIImage imageNamed:@"share.png"] forState:UIControlStateNormal];
-    self.graffitiView.layer.borderWidth = 2;
-    self.graffitiView.layer.borderColor = [UIColor colorWithRed:230/255.0 green:230/255.0 blue:230/255.0 alpha:1].CGColor;
+    self.Canvas.layer.borderWidth = 2;
+    self.Canvas.layer.borderColor = [UIColor colorWithRed:230/255.0 green:230/255.0 blue:230/255.0 alpha:1].CGColor;
+    [self initColor];
+}
+- (void)initColor
+{
+    self.eraser.layer.borderWidth = 1;
+    self.eraser.layer.borderColor = [UIColor blackColor].CGColor;
+    self.eraser.layer.cornerRadius = 4;
+}
+-(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    bMoved=NO;
+    UITouch * touch=[touches anyObject];
+    pntBegin=[touch locationInView:self.myView];
+}
+
+-(void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    
+    bMoved=YES;
+    UITouch * touch=[touches anyObject];
+    CGPoint currentPoint=[touch locationInView:self.myView];
+    CGContextRef context=NULL;
+    
+    UIGraphicsBeginImageContext(self.Canvas.frame.size);
+    [self.Canvas.image drawInRect:self.Canvas.frame];
+    context=UIGraphicsGetCurrentContext();
+    CGContextSaveGState(context);
+    
+    if(bRubber)
+    {
+        CGContextClearRect(context, CGRectMake(currentPoint.x-fBrushWidth,
+                                               currentPoint.y-fBrushWidth,
+                                               fBrushWidth*2,
+                                               fBrushWidth*2));
+    }
+    else
+    {
+        CGContextMoveToPoint(context, pntBegin.x, pntBegin.y);
+        CGContextAddLineToPoint(context, currentPoint.x, currentPoint.y);
+        CGContextSetLineCap(context, kCGLineCapRound);
+        CGContextSetLineWidth(context, fBrushWidth);
+        CGContextSetBlendMode(context, kCGBlendModeNormal);
+        CGContextSetRGBStrokeColor(context, fRed, fGreen, fBlue, 1.0f);
+        CGContextStrokePath(context);
+    }
+    CGContextRestoreGState(context);
+    self.Canvas.image=UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    pntBegin=currentPoint;
+}
+-(void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    UITouch * touch=[touches anyObject];
+    CGPoint currentPoint=[touch locationInView:self.myView];
+    if(!bMoved)
+    {
+        UIGraphicsBeginImageContext(self.Canvas.frame.size);
+        [self.Canvas.image drawInRect:self.Canvas.frame];
+        CGContextRef context=UIGraphicsGetCurrentContext();
+        if(bRubber)
+        {
+            CGContextClearRect(context, CGRectMake(currentPoint.x-fBrushWidth,
+                                                   currentPoint.y-fBrushWidth,
+                                                   fBrushWidth*2,
+                                                   fBrushWidth*2));
+        }
+        else
+        {
+            CGContextMoveToPoint(context, pntBegin.x, pntBegin.y);
+            CGContextAddLineToPoint(context, currentPoint.x, currentPoint.y);
+            CGContextSetLineCap(context, kCGLineCapRound);
+            CGContextSetLineWidth(context, fBrushWidth);
+            CGContextSetBlendMode(context, kCGBlendModeNormal);
+            CGContextSetRGBStrokeColor(context, fRed, fGreen, fBlue, 1.0f);
+            CGContextStrokePath(context);
+        }
+        CGContextFlush(context);
+        self.Canvas.image=UIGraphicsGetImageFromCurrentImageContext();
+        UIGraphicsEndImageContext();
+    }
 }
 
 - (void)didReceiveMemoryWarning
@@ -42,5 +130,64 @@
 - (IBAction)graffitiCancel:(id)sender
 {
     [self dismissViewControllerAnimated:YES completion:nil];
+}
+- (IBAction)actionD:(id)sender
+{
+    bRubber=NO;
+    fRed=102.0f/255.0f;
+    fGreen=208.0f/255.0f;
+    fBlue=0.0f/255.0f;
+}
+- (IBAction)acitonShare:(id)sender
+{
+    
+}
+
+- (IBAction)actionSave:(id)sender
+{
+    UIImageWriteToSavedPhotosAlbum([self.Canvas image], nil, nil, nil);
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"存储涂鸦成功"
+                                                    message:@"已将涂鸦存储于图片库中，打开照片程序即可查看。"
+                                                   delegate:self
+                                          cancelButtonTitle:@"OK"
+                                          otherButtonTitles:nil];
+    [alert show];
+}
+
+- (IBAction)buttonEraser:(id)sender
+{
+     bRubber=YES;
+}
+
+- (IBAction)buttonM:(id)sender
+{
+    bRubber=NO;
+    fRed=0.0f/255.0f;
+    fGreen=0.0f/255.0f;
+    fBlue=0.0f/255.0f;
+}
+
+- (IBAction)buttonA:(id)sender
+{
+    bRubber=NO;
+    fRed=238.0f/255.0f;
+    fGreen=15.0f/255.0f;
+    fBlue=10.0f/255.0f;
+}
+
+- (IBAction)buttonE:(id)sender
+{
+    bRubber=NO;
+    fRed=228.0f/255.0f;
+    fGreen=186.0f/255.0f;
+    fBlue=10.0f/255.0f;
+}
+
+- (IBAction)actionR:(id)sender
+{
+    bRubber=NO;
+    fRed=87.0f/255.0f;
+    fGreen=156.0f/255.0f;
+    fBlue=230.0f/255.0f;
 }
 @end
